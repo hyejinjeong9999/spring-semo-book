3b67be2f5027e5cdc100bcc916817028c8df0955
hyunho058 <hyunho058@naver.com>
2021-12-03 23:29:33 +0900

    SEMO-171 REFACTOR: Update log in PerformanceAspect

--- src/main/java/com/semobook/aop/PerformanceAspect.java
+++ src/main/java/com/semobook/aop/PerformanceAspect.java
@@ -13,17 +13,18 @@ public class PerformanceAspect {
 
 //    @Around("execution(* com.semobook..*.UserController.*(..))")  //포인트컷 표현식을 이용한 방법
     @Around("@annotation(com.semobook.aop.PerformanceCheck)") //annotation을 이용한 방법
-    public Object calculatePerformanceTime(ProceedingJoinPoint proceedingJoinPoint) {
-        Object result = null;
+    public Object calculatePerformanceTime(ProceedingJoinPoint joinPoint)  throws Throwable{
         try {
             long start = System.currentTimeMillis();
-            result = proceedingJoinPoint.proceed();
+            Object result = joinPoint.proceed();
             long end = System.currentTimeMillis();
-            log.info("수행 시간 {} ms", end - start);
-        } catch (Throwable throwable) {
-            log.info("Exception");
-            throwable.printStackTrace();
+            log.info("[PerformanceTime] 수행 시간 = {} ms", end - start);
+            return result;
+        } catch (Exception e) {
+            log.info("[PerformanceTime] Exception = {}",joinPoint.getSignature());
+            throw e;
+        }finally {
+            log.info("[PerformanceTime] {}",joinPoint.getSignature());
         }
-        return result;
     }
 }
