61bcaeace41888797e0d4d082008109b795e3354
hyunho058 <hyunho058@naver.com>
2021-12-16 19:51:56 +0900

    SEMO-171 REFACTOR: Change HandlerInterceptor in Interceptor

--- src/main/java/com/semobook/intercepter/Interceptor.java
+++ src/main/java/com/semobook/intercepter/Interceptor.java
@@ -1,8 +1,8 @@
 package com.semobook.intercepter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,8 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import static com.semobook.common.SemoConstant.SEMO_BOOK_API_AUTHORIZATION;
 
 @Slf4j
-public class Interceptor extends HandlerInterceptorAdapter {
-
+public class Interceptor implements HandlerInterceptor {
     //컨트롤러에 도착하기전에 동작하는 메소드로 return값이 true면 진행, false면 stop
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -24,16 +23,16 @@ public class Interceptor extends HandlerInterceptorAdapter {
             log.info("Authorization fail = {}", authorization);
             return false;
         }
-        return super.preHandle(request, response, handler);
+        return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        super.postHandle(request, response, handler, modelAndView);
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        super.afterCompletion(request, response, handler, ex);
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 }
